acad57b1d4caa0656c4979c4648158ff94d99251
Joe Jalbert <joejalbert29@gmail.com>
2015-04-10 14:41:14 -0400

    Added an indicator to show which node is active. Left click / Right click to navigate.

---
+++ Assets/Scripts/Player/playerPosition.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerPosition : MonoBehaviour {
+
+	public int currentID;
+	public GameObject[] nodes;
+
+	void Start () {
+		nodes = GameObject.FindGameObjectsWithTag("Node");
+	}
+
+	void Update () {
+		nodes = GameObject.FindGameObjectsWithTag("Node");
+
+		transform.position = nodes[currentID].transform.position;
+		if(Input.GetMouseButtonDown(0) && currentID < nodes.Length - 1){
+			currentID ++;
+		}
+		else if(Input.GetMouseButtonDown (1) && currentID > 0){
+			currentID --;
+		}
+	}
+}
