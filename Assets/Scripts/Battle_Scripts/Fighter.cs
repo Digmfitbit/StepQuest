09cdad0cf4717eeb0b852f2b82509ffe6d8a603a
Jonas Schell <jonasschell@me.com>
2015-04-21 14:18:33 -0400

    Added everything needed for Battle. Added Class Fighter and inherited classes Player and Enemy

---
+++ Assets/Scripts/Battle_Scripts/Fighter.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fighter : MonoBehaviour {
+
+	protected float strength, strengthMax;
+	protected float stamina, staminaMax;
+	protected float health, healthMax;
+	protected float recovery,recoveryMax;
+	protected float damage, damageMax;
+
+	protected string fighterName;
+
+	protected bool selected = false;
+	public bool Selected
+	{
+		get{
+			return selected;
+		}
+		set{
+			selected = value;
+		}
+	}
+
+
+	
+	protected virtual void Start () 
+	{
+	
+	}
+	
+	protected virtual void Update ()
+	{
+//		checkSelection ();
+
+	
+	}
+
+	protected virtual void attack(float damageOut)
+	{
+
+	}
+
+	protected virtual void hit(float damageIn)
+	{
+
+	}
+
+	protected virtual void winFight()
+	{
+	}
+
+	protected virtual void loseFight()
+	{
+
+	}
+	
+
+	protected virtual void OnMouseDown()
+	{
+		Debug.Log ("I'm inside of OnMouseDown and my Name is: "+ gameObject.name);
+	}
+
+}
