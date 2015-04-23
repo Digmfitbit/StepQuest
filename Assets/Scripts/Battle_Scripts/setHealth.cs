6a7639fc37797eac7992c3b4b67d68d8d5930f90
Joe Muoio <jgm55@drexel.edu>
2015-04-23 14:55:02 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/Digmfitbit/cyberpunk""
    This reverts commit c62096856dd1ecb5405a08e32db371c0ef55b8f2.

---
+++ Assets/Scripts/Battle_Scripts/setHealth.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setHealth : MonoBehaviour {
+
+	private TextMesh textToDisplay;
+	// Use this for initialization
+	void Start () 
+	{
+		textToDisplay = gameObject.GetComponent<TextMesh> ();
+	}
+	
+	public void setText(string _textToDisplay)
+	{
+		textToDisplay.text = _textToDisplay;
+	}
+}
