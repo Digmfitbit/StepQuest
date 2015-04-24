4fecb0d8cb91018b46cc19b30460aaba2907b134
Joe Jalbert <joejalbert29@yahoo.com>
2015-04-24 18:53:46 -0400

    Added seed to town generation. Text above town now displays it's name.

--- Assets/Scripts/map/townGen.cs
+++ Assets/Scripts/map/townGen.cs
@@ -6,8 +6,18 @@ public class townGen : MonoBehaviour {
 	private string[] townNames = new string[]{"Morcote", "Grungedich", "Heltower", "Marshen", "Ringmer", "Wormsdump", "Carsby", "Someringdene", "Wrongdump", "Alderster", "Grimehovel", "Hurttump",	"Meatylis", "Fulenhill", "Illsneke", "Pontfan", "Sutscastle", "Stenchdump", "Bexsey", "Bournetock",	"Macminster", "Balagate", "Poldock", "Smellbane", "Puckhenge", "Caermore", "Bredean", "Stinkford", "Wormsylis", "Meatester", "Bledtease", "Wealdtown", "Evilerscrutch", "Biteter", "Belysport", "Leverton", "Careter", "Porthster", "Fulthwaite", "Witchcrack", "Billeter", "Baleter", "Carsham", "Bexgate", "Mousebugga", "Duncanvy", "Holmham", "Aldfos", "Knightfield", "Lhanwardine", "Moorend", "Fleshech", "Wealdbury", "Vilewood", "Vileclapp", "Dinaswich", "Culcester", "Shipsay", "Rockdump", "Carnoflea", "Bonefold", "Nantlock", "Shibug", "Biteclapp", "Winterbridge", "Treburgh", "Frogfast", "Enderdale", "Palmside", "Balbryde", "Nastichute", "Nastidich", "Puckoch", "Elslot", "Grimcrack", "Uglysneke", "Belyshaw", "Deverton", "Smellbane", "Fatllyn", "Evilfold", "Frogoflea", "Mousemede", "Somercester", "Impsmarshe", "Dripslip", "Cunkne", "Lewdon", "Mynyddwardine", "Tillypool", "Bourneton", "Brigwick", "Dalworth", "Rotwind", "Coldcatt", "Nantlock", "Grotslide", "Witchach", "Exeberry", "Wrongshagg"};
 	public string townName;
 
+	public GameObject townText;
+
+	public int seed;
+	private System.Random rand;
+
 	void Start () {
-		townName = townNames[Random.Range (0,townNames.Length)];
+		seed = GetComponentInParent<branchMapGen>().u_id;
+		rand = new System.Random(seed);
+		townName = townNames[rand.Next (0,townNames.Length)];
+
+		townText = transform.FindChild ("name_text").gameObject;
+		townText.GetComponent<TextMesh>().text = townName;
 	}
 
 }
