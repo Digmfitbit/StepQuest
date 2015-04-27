126d261f152260d653a5df6353a192b8741b776a
Joe Muoio <jgm55@drexel.edu>
2015-04-27 13:41:11 -0400

    Anchored Title screen, fixed fireball and sprites to render properly on mobile

--- Assets/Scripts/Battle_Scripts/DragonScript.cs
+++ Assets/Scripts/Battle_Scripts/DragonScript.cs
@@ -5,7 +5,6 @@ public class DragonScript : MonoBehaviour {
 
     public GameObject target;
     public GameObject fireball;
-    private Vector3 fireballLocalPos = new Vector3(-.34f,.134f,0f);
 
     bool fired = false;
     GameObject fireBallInstance;
@@ -22,7 +21,7 @@ public class DragonScript : MonoBehaviour {
     public void ShootTarget()
     {
         fired = true;
-        fireBallInstance = Instantiate(fireball,this.transform.position + fireballLocalPos, this.transform.rotation) as GameObject;
+        fireBallInstance = Instantiate(fireball) as GameObject;
         fireBallInstance.GetComponent<FireballController>().setTarget(target);
     }
 
