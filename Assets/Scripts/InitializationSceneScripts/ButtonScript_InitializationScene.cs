17a0605a590bc2b8019f1c884c980024dd8ad2b8
Jonas Schell <jonasschell@me.com>
2015-06-01 21:05:25 -0400

    Added a initialization scene to select a player model and name;

---
+++ Assets/Scripts/InitializationSceneScripts/ButtonScript_InitializationScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript_InitializationScene : MonoBehaviour {
+
+	public void OnSet_Button () 
+	{
+		GameObject.Find ("InitializationManager").SendMessage ("SetEverthing");
+	}
+
+	public void OnCharacterDown_Button()
+	{
+		GameObject.Find ("InitializationManager").SendMessage ("SetSelected", gameObject.name);
+	}
+}
---
+++ Assets/Scripts/InitializationSceneScripts/InizializationManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class InizializationManager : MonoBehaviour {
+
+	PlayerStats mainPlayer;
+	string selectedCharacter = null;
+	string playerName = null;
+	public InputField inputPlayerName = null;
+
+	void Awake () 
+	{
+		mainPlayer = FindObjectOfType<PlayerManager> ().mainPlayer;
+	}
+
+	public void SetSelected(string _name)
+	{
+		selectedCharacter = _name;
+		Debug.Log (selectedCharacter + "was selected!");
+	}
+
+	public void SetEverthing()
+	{
+		if (selectedCharacter != null)
+			mainPlayer.playerClassID = selectedCharacter;
+		else
+			Debug.LogWarning ("Select a Player befor continuing");
+
+		if(player
+	}
+
+	public void GetPlayerName()
+	{
+		playerName = inputPlayerName.text;
+		Debug.Log (playerName);
+	}
+}
